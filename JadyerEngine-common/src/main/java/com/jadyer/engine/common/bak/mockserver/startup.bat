4ccf7202b13388451f494deabf5d0e111ad3df1a
jadyer <jadyer@yeah.net>
2015-10-05 17:14:55 +0800

    remove mockserver package and code

--- JadyerEngine-common/src/main/java/com/jadyer/engine/common/bak/mockserver/startup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title OpenAPI MockServer¡¾ÐþÓñÖÆ×÷¡¿
-color 02
-REM https://repo1.maven.org/maven2/com/github/dreamhead/moco-runner/0.10.2/moco-runner-0.10.2-standalone.jar
-REM java -jar moco-runner-0.10.2-standalone.jar http -p 8082 -c openapi.json
-java -jar moco-runner-0.10.2-standalone.jar start -p 8082 -g whole.json
